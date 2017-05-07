a7348e2bbd8901d4f23f53a1a243def98bae0eb9
LennyThompson <LennyThompson@gmail.com>
2017-05-08 08:23:39 +1000

    Removing deprectaed components

--- src/app/components/survey/form/traverse/traverse-form.ts
+++ src/app/components/survey/form/traverse/traverse-form.ts
@@ -4,6 +4,9 @@
 import {Component, OnInit, Input} from "@angular/core";
 
 import {Traverse} from "./../../../../services/surveyDb/types";
+import {TraverseMeasurementSummary} from "../../../../services/surveyDb/types/TraverseMeasurementSummary";
+import {Observable} from "rxjs";
+import {TraverseMeasurementSummarySubjectProvider} from "../../../../services/surveyDb/webAPI/traverse-measurement-summary-service/TraverseMeasurementSummarySubjectProvider";
 
 @Component (
     {
@@ -15,7 +18,7 @@ import {Traverse} from "./../../../../services/surveyDb/types";
 export class TraverseComponent implements OnInit
 {
     private _Traverse: Traverse;
-    constructor()
+    constructor(private _TarvserMeasurementSummary: TraverseMeasurementSummarySubjectProvider)
     {
         this._Traverse = new Traverse();
     }
@@ -33,5 +36,14 @@ export class TraverseComponent implements OnInit
     get Traverse(): Traverse
     {
         return this._Traverse;
+    }
+
+    get traverseSummary(): Observable<TraverseMeasurementSummary>
+    {
+        if(this._Traverse && this._Traverse.ID > 0)
+        {
+            return this._TarvserMeasurementSummary.getTraverseMeasurementSummarySummary();
+        }
+        return new Observable<TraverseMeasurementSummary>(null);
     }
 }
